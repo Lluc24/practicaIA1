7c8529d65e5ef3662ddd4fa108349144fd7e2891
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-10-13 18:15:45 +0200

    Main completo

--- src/com/lluc/practicaIA1/Main.java
+++ src/com/lluc/practicaIA1/Main.java
@@ -26,6 +26,7 @@ public class Main {
             }
         });
         Estado inicial = new Estado(paquetes, transporte);
+        // Seleccionar uno
         azamonHillClimbingSearch(inicial);
         azamonSimulatedAnnealingSearch(inicial);
     }
